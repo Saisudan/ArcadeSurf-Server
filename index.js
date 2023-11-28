ae2c850322821d3dd3f0c6f7917fde2bd1b75f59
Saisudan Bashkaran <saisudan.b@gmail.com>
2023-11-27 19:40:15 -0500

    added some events for handling ghost sprites

--- index.js
+++ index.js
@@ -105,10 +105,15 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("won-game", async (receivedData) => {
+  socket.on("won-game", (receivedData) => {
     const { roomID, username } = receivedData;
     socket.to(roomID).emit("lost-game");
-  })
+  });
+
+  socket.on("current-player-position", (receivedData) => {
+    const { room } = receivedData;
+    socket.to(room).emit("other-player-positions", receivedData);
+  });
 
   socket.on("disconnect", () => {
     console.log(`${socket.id} disconnected`)
